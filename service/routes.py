95a0c779d420058c7d527d4b8d529e3fe752f693
olegkishko <oleg.kishko@gmail.com>
2024-12-30 11:49:31 -0500

    Added badge for GitHub Actions

--- service/routes.py
+++ service/routes.py
@@ -75,7 +75,6 @@ def list_accounts():
 
     return jsonify(account_list), status.HTTP_200_OK
 
-
 ######################################################################
 # READ AN ACCOUNT
 ######################################################################
@@ -90,7 +89,7 @@ def get_accounts(account_id):
     account = Account.find(account_id)
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
-    
+
     return account.serialize(), status.HTTP_200_OK
 
 ######################################################################
@@ -113,7 +112,6 @@ def update_accounts(account_id):
 
     return account.serialize(), status.HTTP_200_OK
 
-
 ######################################################################
 # DELETE AN ACCOUNT
 ######################################################################
@@ -132,7 +130,6 @@ def delete_accounts(account_id):
     account.delete()
     
     return "", status.HTTP_204_NO_CONTENT
-
 
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
